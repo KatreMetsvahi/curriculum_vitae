6c24f919d3078bb56a0306ff2fb4e980ab62e094
Katre Metsvahi <katre.metsvahi@i-smith.ee>
2020-03-28 09:12:19 +0200

    Update cloud styles

--- src/components/Clouds.jsx
+++ src/components/Clouds.jsx
@@ -14,9 +14,9 @@ const Clouds = () => {
             key={group * CLOUD_GROUP_COUNT + index}
             style={{
                 animationDelay: group === 0 ? 0 : `${getRandomNumber(10 * (group - 1), 10 * group)}s`,
-                animationDuration: `${getRandomNumber(30, 90)}s`,
-                top: `${getRandomNumber(10, 60)}%`,
-                transform: `scale(.${getRandomNumber(1, 6)})`
+                animationDuration: `${getRandomNumber(30, 120)}s`,
+                top: `${getRandomNumber(10, 80)}%`,
+                transform: `scale(.${getRandomNumber(1, 8)})`
             }}
         />
     );
