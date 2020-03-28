0ac519c2ababf4176e87a759a891e1c4e813cfbb
Katre Metsvahi <katre.metsvahi@i-smith.ee>
2020-03-28 09:58:12 +0200

    Unify spaces

--- src/components/Clouds.tsx
+++ src/components/Clouds.tsx
@@ -5,27 +5,27 @@ import { getArrayOfEmptyItems } from '../utils/arrayUtils';
 const CLOUD_GROUP_COUNT = 4;
 
 const Clouds = () => {
-    const cloudCount = Math.ceil(window.innerWidth / 100);
-    const cloudArray = getArrayOfEmptyItems(cloudCount);
+  const cloudCount = Math.ceil(window.innerWidth / 100);
+  const cloudArray = getArrayOfEmptyItems(cloudCount);
 
-    const generateCloud = (group: number, index: number): JSX.Element => (
-        <div
-            className={'cloud'}
-            key={group * CLOUD_GROUP_COUNT + index}
-            style={{
-                animationDelay: group === 0 ? '0' : `${getRandomNumber(10 * (group - 1), 10 * group)}s`,
-                animationDuration: `${getRandomNumber(30, 120)}s`,
-                top: `${getRandomNumber(10, 80)}%`,
-                transform: `scale(.${getRandomNumber(1, 8)})`
-            }}
-        />
-    );
+  const generateCloud = (group: number, index: number): JSX.Element => (
+    <div
+      className={'cloud'}
+      key={group * CLOUD_GROUP_COUNT + index}
+      style={{
+        animationDelay: group === 0 ? '0' : `${getRandomNumber(10 * (group - 1), 10 * group)}s`,
+        animationDuration: `${getRandomNumber(30, 120)}s`,
+        top: `${getRandomNumber(10, 80)}%`,
+        transform: `scale(.${getRandomNumber(1, 8)})`
+      }}
+    />
+  );
 
-    return (
-        <React.Fragment>
-            { cloudArray.map((_, index) => generateCloud(index % CLOUD_GROUP_COUNT, index)) }
-        </React.Fragment>
-    );
+  return (
+    <React.Fragment>
+      {cloudArray.map((_, index) => generateCloud(index % CLOUD_GROUP_COUNT, index))}
+    </React.Fragment>
+  );
 };
 
 export default Clouds;
--- src/layout/Header.tsx
+++ src/layout/Header.tsx
@@ -2,12 +2,12 @@ import React from 'react';
 import Clouds from '../components/Clouds';
 
 const Header = () => (
-    <header>
-        <h1>Katre Metsvahi</h1>
-        <h2>Curriculum Vitae</h2>
+  <header>
+    <h1>Katre Metsvahi</h1>
+    <h2>Curriculum Vitae</h2>
 
-        <Clouds/>
-    </header>
+    <Clouds/>
+  </header>
 );
 
 export default Header;
