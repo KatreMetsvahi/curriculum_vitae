b4bdbe23892827cc05e1cb14865b642ddeebb3de
Katre Metsvahi <katre.metsvahi@i-smith.ee>
2020-03-30 16:49:38 +0300

    Add more color to about me

--- src/layout/AboutMe.jsx
+++ src/layout/AboutMe.jsx
@@ -8,18 +8,18 @@ const AboutMe = () => {
 
       <div className={'grid'}>
         <div className={'box box--with-icon'}>
-          <i className="material-icons box__icon">star</i>
+          <i className="material-icons box__icon box__icon--yellow">star</i>
           <p>{ aboutme.intro }</p>
         </div>
 
         <div className={'box box--with-icon'}>
-          <i className="material-icons box__icon">favorite</i>
+          <i className="material-icons box__icon box__icon--red">favorite</i>
           <p>{ aboutme.passion1 }</p>
           <p>{ aboutme.passion2 }</p>
         </div>
 
         <div className={'box box--with-icon'}>
-          <i className="material-icons box__icon">sports_tennis</i>
+          <i className="material-icons box__icon box__icon--blue">sports_tennis</i>
           <p>{ aboutme.hobbies }</p>
         </div>
       </div>
