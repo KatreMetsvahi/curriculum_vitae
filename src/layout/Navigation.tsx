fc8f1636f3863fd12bc6d66413ce2e0a2c1bb2bf
Katre Metsvahi <katre.metsvahi@gmail.com>
2020-03-31 21:11:44 +0300

    Improve navigation style

--- src/layout/Navigation.tsx
+++ src/layout/Navigation.tsx
@@ -1,48 +1,61 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import cx from 'classnames';
 import t from '../utils/translator';
 
-const Navigation = () => (
-  <nav>
-    <a
-      className={'nav-link nav-link--cloud'}
-      href={'#clouds'}
-    >
-      <i className={'fas fa-cloud nav-link__icon'}/>
-      <span className={'nav-link__text'}>{ t('top') }</span>
-    </a>
+const Navigation = () => {
+  const [pageScrolled, setPageScrolled] = useState(false);
 
-    <a
-      className={'nav-link'}
-      href={'#work'}
-    >
-      <i className={'fas fa-briefcase nav-link__icon'}/>
-      <span className={'nav-link__text'}>{ t('work.menuItem') }</span>
-    </a>
+  const handleScroll = () => {
+    setPageScrolled(window.scrollY !== 0);
+  };
 
-    <a
-      className={'nav-link'}
-      href={'#education'}
-    >
-      <i className={'fas fa-graduation-cap nav-link__icon'}/>
-      <span className={'nav-link__text'}>{ t('education.menuItem') }</span>
-    </a>
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+  }, []);
 
-    <a
-      className={'nav-link'}
-      href={'#about-me'}
-    >
-      <i className={'fas fa-address-card nav-link__icon'}/>
-      <span className={'nav-link__text'}>{ t('aboutMe.menuItem') }</span>
-    </a>
+  return (
+    <nav className={cx('nav', { 'nav--scrolled': pageScrolled })}>
+      <a
+        className={'nav-link nav-link--cloud'}
+        href={'#clouds'}
+      >
+        <i className={'fas fa-cloud nav-link__icon'}/>
+        <span className={'nav-link__text'}>{ t('top') }</span>
+      </a>
 
-    <a
-      className={'nav-link'}
-      href={'#contacts'}
-    >
-      <i className={'fas fa-envelope nav-link__icon'}/>
-      <span className={'nav-link__text'}>{ t('contacts.menuItem') }</span>
-    </a>
-  </nav>
-);
+      <a
+        className={'nav-link'}
+        href={'#work'}
+      >
+        <i className={'fas fa-briefcase nav-link__icon'}/>
+        <span className={'nav-link__text'}>{ t('work.menuItem') }</span>
+      </a>
+
+      <a
+        className={'nav-link'}
+        href={'#education'}
+      >
+        <i className={'fas fa-graduation-cap nav-link__icon'}/>
+        <span className={'nav-link__text'}>{ t('education.menuItem') }</span>
+      </a>
+
+      <a
+        className={'nav-link'}
+        href={'#about-me'}
+      >
+        <i className={'fas fa-address-card nav-link__icon'}/>
+        <span className={'nav-link__text'}>{ t('aboutMe.menuItem') }</span>
+      </a>
+
+      <a
+        className={'nav-link'}
+        href={'#contacts'}
+      >
+        <i className={'fas fa-envelope nav-link__icon'}/>
+        <span className={'nav-link__text'}>{ t('contacts.menuItem') }</span>
+      </a>
+    </nav>
+  )
+};
 
 export default Navigation;
